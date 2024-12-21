cc08b6df9f84b1fc4ed198512608b56ef856eaf6
DangoMelon <gerardo_art@me.com>
2024-12-21 05:03:06 -0500

    Add .gitignore

--- .ipynb_checkpoints/run_tmux-checkpoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-module load conda
-conda activate /glade/work/griverat/conda-envs/keras
-
-for i in {0..7}; do
-    tmux new-session -d -s load$i "papermill get_data.ipynb output/get_data_$i.ipynb -p n $i -k keras"
-done
